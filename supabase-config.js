d3582ce8bcd2741ba6538fa2a582fe9a5dd5ff5c
Klay <vitormiguelrodrigues79@gmail.com>
2026-08-16 18:12:02 +0100

    delete

--- supabase-config.js
+++
@@ -1,3 +0,0 @@
-export const SUPABASE_URL = "https://boyhtywhuumbayejfbse.supabase.co";
-export const SUPABASE_KEY = "sb_publishable_U3BQ__QzzsBOSq6w_2LGew_GczKnjhO";
-export const SUPABASE_BUCKET = "watch-photos";
