7f1762e2b188e7af493f109c85f16290ce51e071
Klay <vitormiguelrodrigues79@gmail.com>
2026-08-16 12:40:01 +0100

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = "https://boyhtywhuumbayejfbse.supabase.co";
+export const SUPABASE_KEY = "sb_publishable_U3BQ__QzzsBOSq6w_2LGew_GczKnjhO";
+export const SUPABASE_BUCKET = "watch-photos";
